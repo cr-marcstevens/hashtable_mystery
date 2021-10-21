0d6cd3cbc5b0e815d6c7ff540d23c9bced002f45
cr-marcstevens <github@marc-stevens.nl>
2021-10-21 14:28:21 +0200

    Added my binaries

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CXXFLAGS="-std=c++11 -O2 -march=native -falign-functions=64"
+CXXFLAGS="-std=c++11 -O2 -march=native -falign-functions=64 -static"
 
 mkdir -p data
 
@@ -14,14 +14,14 @@ for CXX in g++; do
 	echo "CXX=$CXX    CXXFLAGS=$CXXFLAGS"
 	$CXX --version > data/${CXX}_version.txt
 
-	$CXX $CXXFLAGS main.cpp -o test
-	./test |& tee data/${CXX}_binary.txt
+	$CXX $CXXFLAGS main.cpp -o test_binary
+	./test_binary |& tee data/${CXX}_binary.txt
 	objdump ./test --disassemble=hash_table_ok  > data/${CXX}_binary_ok.asm
 	objdump ./test --disassemble=hash_table_bad > data/${CXX}_binary_bad.asm
 	objdump ./test --disassemble=hash_table_alt > data/${CXX}_binary_alt.asm
 
-	$CXX $CXXFLAGS main.cpp -o test -DUSE_PRIME
-	./test |& tee data/${CXX}_prime.txt
+	$CXX $CXXFLAGS main.cpp -o test_prime -DUSE_PRIME
+	./test_prime |& tee data/${CXX}_prime.txt
 	objdump ./test --disassemble=hash_table_ok  > data/${CXX}_prime_ok.asm
 	objdump ./test --disassemble=hash_table_bad > data/${CXX}_prime_bad.asm
 	objdump ./test --disassemble=hash_table_alt > data/${CXX}_prime_alt.asm
